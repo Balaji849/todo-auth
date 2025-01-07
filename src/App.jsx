e54ce23cd135531c35342a7144eb5949bea97151
balaji <balajic721@gmail.com>
2025-01-07 13:33:43 +0530

    wd

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import Card from "../src/components/Todocard";
+import Loader from "./components/Loader";
+import logo from "./assets/logo.png"
+import { SignedIn, SignedOut, SignInButton, UserButton,useUser,useAuth } from "@clerk/clerk-react";
+import Intro from "./components/Intro";
+
+
+const firebaseUrl ='https://front-end-cohort-default-rtdb.asia-southeast1.firebasedatabase.app/'
+
+function App() {
+  let {user}=useUser();
+  let {isSignedIn}=useAuth();
+  let taskinput=useRef(null)
+  let[formStatus,setformStatus]=useState(false)
+  let[todos,setTodos]=useState([])
+
+  function handleSubmit(){
+    setformStatus(true)
+    let task=taskinput.current.value;
+    axios.post(`${firebaseUrl}todos.json`,
+      {
+        title:task,
+        createdBy:user.username,
+      }
+    ).then(()=>{
+      setformStatus(false)
+      fetchTodos();
+    })
+    
+  }
+  
+  function handleDelete(id){
+    axios.delete(`${firebaseUrl}todos/${id}.json`).then(()=>{
+      fetchTodos();
+    })
+  }
+  function fetchTodos(){
+    axios.get(`${firebaseUrl}todos.json`).then(todos=>{
+      let tempTodos=[];
+      for (let key in todos.data){
+        let todo={
+          id:key,
+          ...todos.data[key]
+        }
+        tempTodos.push(todo)
+      }
+      setTodos(tempTodos)
+    })
+  }
+
+  
+  useEffect(()=>{
+    fetchTodos()
+  },[])
+
+  return (
+    <>
+    <div className=" border-b py-3 ">
+    <div className="max-w-4xl mx-auto flex justify-between items-center">
+      <img className="h-8" src={logo} alt="" />
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </div>
+    </div>
+
+    <SignedIn>
+      <div  className="w-[500px] mx-auto mt-12">
+      <h1 className="text-2xl font-bold">Manage your Taks!<span className="text-neutral-500">{isSignedIn ?user.fullName:""}</span></h1>
+      <p>Empowering productivity with seamless task management</p>
+      <input ref={taskinput}  className="mt-2 border rounded-xl p-3 w-[300px]  focus:outline-none border-neutral-300" type="text" placeholder="Add Tasks i.e Learn Blockchain" />
+
+      <button onClick={handleSubmit} className="mt-2 rounded-xl py-3 px-5 bg-black text-white flex align-center gap-4">
+        {!formStatus ? "Create Task" : <Loader/>}
+      </button>
+
+      <div className="mt-12">
+     {todos.filter(todo=>isSignedIn? todo.createdBy==user.username :true).map(todo=> <Card title={todo.title} handleDelete={handleDelete} id={todo.id} key={todo.id} />) }
+      </div>
+
+      </div>
+      </SignedIn>
+      
+      <SignedOut>
+        <Intro/>
+      </SignedOut>
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Intro.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Button } from "@/components/ui/button";
+import BoxReveal from "@/components/ui/box-reveal";
+import pic from "../assets/pic.jpg"
+function Intro() {
+  return (
+    <>
+    <div className='mt-36 mx-auto max-w-xl text-center'>
+    <div className="">
+    <BoxReveal boxColor={"#757576"} duration={0.5}>
+             <img src={pic} className='h-40 inline ml-56 ' alt="" />
+      </BoxReveal>
+        </div>    
+   
+        
+       
+    <div className="size-full max-w-lg items-center justify-center overflow-hidden pt-8 text-center inline-block">
+      <BoxReveal boxColor={"#757576"} duration={0.5}>
+        <p className="text-[3.5rem] font-semibold ">
+          TASKY<span className="text-[#000000]">.</span>
+        </p>
+      </BoxReveal>
+ 
+      <BoxReveal boxColor={"#757576"} duration={0.5}>
+        <h2 className="mt-[.5rem] text-[1rem] font-semibold">
+        Streamline Your Day, Effortlessly.{" "}
+          <span className="text-[#757576]">Your Personal To-Do List Manager</span>
+        </h2>
+      </BoxReveal>
+ 
+      <BoxReveal boxColor={"#757576"} duration={0.5}>
+        <div className="mt-6">
+          <p className='font-semibold'>
+            -&gt; Prioritize. Organize. Achieve. A seamless platform to track and manage tasks with precision.
+          </p>
+        </div>
+      </BoxReveal>
+ 
+      <BoxReveal boxColor={"#757576"} duration={0.5}>
+        <Button className="mt-[1.6rem] bg-[#000000]">Explore</Button>
+      </BoxReveal>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Intro
